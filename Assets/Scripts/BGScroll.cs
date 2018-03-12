45ef1c20c6bf8b29e7e5841b10b12ceafc6f5e68
tinyu <tinyu45O@163.com>
2018-03-12 22:15:22 +0800

    实现背景滚动(UV),障碍物动态生成

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+
+	public float Speed = 0.005f;
+
+	float count=0; 
+
+
+	void Start()
+	{
+		GeneratePipe ();
+	}
+
+	void FixedUpdate()
+	{
+		if (count++ == 100) 
+		{
+			GeneratePipe ();
+			count = 0;
+		}
+		MeshRenderer render = GetComponent<MeshRenderer> ();
+		render.material.mainTextureOffset -= new Vector2 (Speed, 0);
+	}
+
+
+	/***
+	 * 动态生成水管
+	 * **/
+	void GeneratePipe()
+	{
+		Object obj = Resources.Load ("Pipe");              //加载到内存
+		GameObject pipe = GameObject.Instantiate (obj) as GameObject;    //实例化为游戏对象
+	
+		//设置水管位置：
+		// Min:0.95, Max:2.8
+		pipe.transform.position=new Vector3(pipe.transform.position.x, Random.Range(0.95f, 2.8f), 0);
+
+	}
+}
---
+++ Assets/Scripts/PipeMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMove : MonoBehaviour {
+
+	public float Speed = 0.03f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void Update()
+	{
+		if (transform.position.x < -4) 
+		{
+			Destroy (gameObject);  //销毁游戏对象
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		//水管向左移动
+		transform.position -= new Vector3 (Speed, 0, 0);
+	}
+}
